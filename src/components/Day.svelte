6ed461c615c19b033aca7e003c5b79cb9d441cfd
Cromski <jakobrhenriksen@outlook.com>
2023-07-12 15:07:04 +0200

    event text on days between start and end dates

--- src/components/Day.svelte
+++ src/components/Day.svelte
@@ -7,7 +7,16 @@
     export let month: number
     export let day: number
 
-    $: today = new Date(`${$selectedYearStore} ${$monthsStore[month]} ${day}`).toLocaleDateString("da-dk", {weekday: "short"})
+    $: weekday = new Date(`${$selectedYearStore} ${$monthsStore[month]} ${day}`).toLocaleDateString("da-dk", {weekday: "short"})
+
+    const getUnixTime = (date: Date) => {
+        return Math.floor(date.getTime() / 1000)
+    }
+
+    $: event = events.events.find((e) => 
+    getUnixTime(new Date(`${$selectedYearStore} ${$monthsStore[month]} ${day}`)) >= getUnixTime(new Date(`${e.startDate.split("-")[2]} ${e.startDate.split("-")[1]} ${e.startDate.split("-")[0]}`)) && 
+    getUnixTime(new Date(`${$selectedYearStore} ${$monthsStore[month]} ${day}`)) <= getUnixTime(new Date(`${e.endDate.split("-")[2]} ${e.endDate.split("-")[1]} ${e.endDate.split("-")[0]}`)))?.name
+
 
     const getWeekNumber = () => {
         var date = new Date(`${$selectedYearStore} ${$monthsStore[month]} ${day}`);
@@ -18,25 +27,25 @@
     }
 
     const convertToShorterWeekday = (weekday: string) => {
-        if (weekday === "man.") return "Ma"
+        if (weekday === "man.")       return "Ma"
         else if (weekday === "tirs.") return "Ti"
-        else if (weekday === "ons.") return "On"
+        else if (weekday === "ons.")  return "On"
         else if (weekday === "tors.") return "To"
-        else if (weekday === "fre.") return "Fr"
-        else if (weekday === "lør.") return "Lø"
-        else return "Sø"
+        else if (weekday === "fre.")  return "Fr"
+        else if (weekday === "lør.")  return "Lø"
+        else                          return "Sø"
     }
 
 </script>
 
 
-<div class={today == "lør." ? ` bg-slate-200 flex w-full h-6` : today == "søn." ? ` bg-slate-400 flex w-full h-6` : ` bg-white flex h-6 w-full border-b-black border-b-[1px]`}>
+<div class={weekday == "lør." ? ` bg-slate-200 flex w-full h-6` : weekday == "søn." ? ` bg-slate-400 flex w-full h-6` : ` bg-white flex h-6 w-full border-b-black border-b-[1px]`}>
     <div class=" min-w-[44px] max-w-[44px] bg-slate-800 text-gray-300 text-xs leading-6">
         <h1 class=" ml-1 inline-block">{day}.</h1>
-        <h1 class=" mr-1 inline-block float-right">{convertToShorterWeekday(today)}</h1>
+        <h1 class=" mr-1 inline-block float-right">{convertToShorterWeekday(weekday)}</h1>
     </div> 
-    <h1 class=" ml-1 mr-auto truncate ">{ events.events.find((e) => e.startDate == (day+"-"+month+"-"+$selectedYearStore).toString())?.name }</h1> <!--  day+"-"+month+"-"+$selectedYearStore -->
-    {#if today == "man."}
+    <h1 class=" ml-1 mr-auto truncate ">{ event == undefined  ? " " : event }</h1> <!--  day+"-"+month+"-"+$selectedYearStore -->
+    {#if weekday == "man."}
         <h1 class=" mr-1 w-4 text-xs">{getWeekNumber()}</h1>
     {/if}
     
