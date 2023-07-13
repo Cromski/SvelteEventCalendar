19981d1f3dd25ff88d1f9b17f58e81beb98d8bdb
Cromski <jakobrhenriksen@outlook.com>
2023-07-13 07:44:27 +0200

    implemented click outside modal to hide it

--- src/components/FullDayModal.svelte
+++ src/components/FullDayModal.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+    import {clickOutside} from '../Extra/clickOutside.js';
+
     import dayModalStore from "../stores/DayModalStore";
+
+    const handleClickOutside = () => {
+        dayModalStore.set({visible: false, event: ""});
+    }
+
+    $: vis = $dayModalStore.visible
 
 </script>
 
-<div class=" absolute bg-white w-[40%] h-[30%] top-32 border-b-black border-[1px] rounded">
+{#if vis}
+<div use:clickOutside on:click_outside={handleClickOutside} class=" absolute bg-white w-[40%] h-[30%] top-32 border-b-black border-[1px] rounded">
     <h1 class="  ">{$dayModalStore.event}</h1>
     <h1 class="  ">{$dayModalStore.event}</h1>
     <h1 class="  ">{$dayModalStore.event}</h1>
 </div>
+{/if}
